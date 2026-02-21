f0800636ca58f43317c04660869d0af300c266c9
JUAN MANUEL <juandev.sp@gmail.com>
2026-02-21 11:08:17 +0100

    update countdowntimer

--- src/app/components/sections/CountdownTimer.tsx
+++ src/app/components/sections/CountdownTimer.tsx
@@ -38,7 +38,7 @@ export default function CountdownTimer({ onSecretWordFound }: CountdownTimerProp
 
   useEffect(() => {
     setMounted(true);
-    const targetDate = new Date('2025-09-15T00:00:00').getTime();
+    const targetDate = new Date('2027-01-15T00:00:00').getTime();
 
     const timer = setInterval(() => {
       const now = new Date().getTime();
